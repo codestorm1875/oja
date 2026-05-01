53b1d6fa1cbcef8a9b2bbd623e6f83baf4b911fe
codestorm1875 <sanyaoluabideen@gmail.com>
2026-05-01 01:29:58 +0100

    feat: wire gateway proxy and engine health

--- gateway/main.go
+++ gateway/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,6 +16,7 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("ok"))
 	})
+	mux.Handle("/engine/", newEngineProxy())
 
 	port := os.Getenv("GATEWAY_PORT")
 	if port == "" {
@@ -28,5 +32,30 @@ func main() {
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
+}
+
+func newEngineProxy() http.Handler {
+	engineURL := os.Getenv("ENGINE_URL")
+	if engineURL == "" {
+		engineURL = "http://localhost:3000"
+	}
+
+	target, err := url.Parse(engineURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	originalDirector := proxy.Director
+
+	proxy.Director = func(req *http.Request) {
+		originalDirector(req)
+		req.URL.Path = strings.TrimPrefix(req.URL.Path, "/engine")
+		if req.URL.Path == "" {
+			req.URL.Path = "/"
+		}
+	}
+
+	return proxy
 }
 
